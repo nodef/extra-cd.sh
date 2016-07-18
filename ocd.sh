a0e58caa0cc127d7b5488d63c612dd632446d8bd
Subhajit Sahu <wolfram77@gmail.com>
2016-07-18 21:28:07 +0530

    ocd: script works ok

---
+++ ocd.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function ocd() {
+
+# prepare
+local d="$HOME/.config/0rez/ocd"
+mkdir -p $d
+
+# options
+local x="0"
+local a="$*"
+local c="${a::1}"
+local f="$d/${a:1:${#a}}.ini"
+if [[ "$a" == "-" ]]; then
+	popd > /dev/null
+elif [[ "$c" == "+" ]]; then
+	pwd > "$f" && x=1
+elif [[ "$c" == "-" ]]; then
+	rm -f "$f" && x=1
+elif [[ "$c" == "=" ]]; then
+	if [[ -f "$f" ]]; then
+		pushd $(cat "$f") > /dev/null
+	else
+		>&2 echo "err: no such shortcut." && x=1
+	fi
+else
+	pushd "$a" > /dev/null
+fi
+if [[ "$x" == "0" ]]; then
+	ls --color --group-directories-first
+	echo
+fi
+}
